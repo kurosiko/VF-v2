25d9f4b95a815774b0cfa6f6f3f207ef73485329
kurosiko <ttnntt1234567890987654321@gmail.com>
2024-07-23 12:33:15 +0900

    Add vitest

--- src/functions/yt_dlp.test.ts
+++ src/functions/yt_dlp.test.ts
@@ -1,3 +1,7 @@
+import { expect, test } from "vitest";
 import { yt_dlp } from "./yt_dlp";
-const test = new yt_dlp(["https://www.youtube.com/watch?v=6JllTO5r7f0"]);
-await test.analyze();
+
+test("download", async () => {
+    const test = new yt_dlp(["https://www.youtube.com/watch?v=6JllTO5r7f0"]);
+    await test.analyze();
+});
